888eb5dcfe13b5c50da8408fe8e5f3c35eebd0db
diascode <thiagotupa@hotmail.com>
2020-01-08 20:53:36 +0000

    project

---
+++ movieapi/src/pages/home/home.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { MovieProvider } from '../../providers/movie/movie';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+    
+  movies: any[]; 
+
+  constructor(public navCtrl: NavController, private mp:MovieProvider) {
+  }
+  ionViewDidLoad(){
+    this.mp.getMovies().subscribe(data =>{
+      this.movies = data.Search;
+    })
+  }
+}
